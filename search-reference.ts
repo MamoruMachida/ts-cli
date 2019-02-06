cc1b6a114d0e520aa08a363e0a4d3e8ccaf9f715
MamoruMachida <mamoru.machida@fox-hound.jp>
2019-02-06 12:56:12 +0900

    add mysql and fee fix

--- search-reference.ts
+++ search-reference.ts
@@ -4,7 +4,7 @@ import * as opn from 'opn';
  * search reference by keyword
  *
  * @param k 検索したい語句
- * @param t 検索するライブラリ support→phalcon
+ * @param t 検索するリファレンス 対応済み:phalcon, php, google, fontawesome, mdn, android, kotlin
  */
 export default function searchReference(k: string = '', t: string = 'default') {
     const tl = t.toLowerCase();
@@ -12,17 +12,18 @@ export default function searchReference(k: string = '', t: string = 'default') {
       tl = alias.get(tl)
     }
     const url = supported[tl].url;
-    opn(`${url}${k}`);
+    return opn(`${url}${k}`);
 }
 
 const supported = {
   default: {url: 'https://www.google.com/search?q='},
   phalcon: {url: 'https://docs.phalconphp.com/search?q='},
   fontawesome: {url: 'https://fontawesome.com/icons?d=gallery&q='},
-  web: {url: 'https://developer.mozilla.org/ja/search?q='},
+  mdn: {url: 'https://developer.mozilla.org/ja/search?q='},
   php: {url: 'http://php.net/manual-lookup.php?pattern='},
   android: {url: 'https://developer.android.com/s/results/?q='},
-  kotlin: {url: 'https://kotlinlang.org/docs/reference/coding-conventions.html?q='}
+  kotlin: {url: 'https://kotlinlang.org/docs/reference/coding-conventions.html?q='},
+  mysql: {url: 'https://www.oracle.com/search/results?cat=mysql&Ntk=SI-ALL5&Ntt='}
 }
 
 const alias = new Map([
